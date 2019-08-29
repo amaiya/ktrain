a3d4e6094f7056ca5afe0f826c47e66d8d0a7eea
Arun Maiya <amaiya@ida.org>
2019-08-29 10:00:00 -0400

    added ner data loader to __init__.py

--- ktrain/text/__init__.py
+++ ktrain/text/__init__.py
@@ -1,8 +1,10 @@
 from .models import *
 from .data import *
+from .ner.data import entities_from_csv
 __all__ = [
            'text_classifier', 
            'print_text_classifiers'
            'texts_from_folder', 'texts_from_csv',
+           'entities_from_csv'
            ]
 
