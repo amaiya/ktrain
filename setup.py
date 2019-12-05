95741552dd08e268f0edda4f449d0b7bcaa26cc1
Arun Maiya <amaiya@ida.org>
2019-12-05 17:21:56 -0500

    update install_requires

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
           'networkx==2.3',
           'bokeh',
           'eli5 >= 0.10.0',
-          'git+ssh://git@github.com/amaiya/stellargraph@sg_ktrain_standalone#egg=stellargraph'
+          'https://github.com/amaiya/stellargraph@sg_ktrain_standalone#egg=stellargraph'
           #'pillow'
       ],
   classifiers=[  # Optional
