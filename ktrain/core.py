0192cc634076d29e0cbda6c9832b7a8ca1a2ce73
Arun Maiya <amaiya@ida.org>
2020-01-19 08:59:22 -0500

    base classes for ktrain.Dataset

--- ktrain/core.py
+++ ktrain/core.py
@@ -1185,12 +1185,50 @@ class GenLearner(Learner):
         return layer_out
 
 
+    #def view_top_losses(self, n=4, preproc=None, val_data=None):
+    #    """
+    #    Views observations with top losses in validation set.
+    #    Musta be overridden by Learner subclasses.
+    #    """
+    #    raise NotImplementedError('view_top_losses must be overriden by GenLearner subclass')
     def view_top_losses(self, n=4, preproc=None, val_data=None):
         """
         Views observations with top losses in validation set.
-        Musta be overridden by Learner subclasses.
+        Typically over-ridden by Learner subclasses.
+        Args:
+         n(int or tuple): a range to select in form of int or tuple
+                          e.g., n=8 is treated as n=(0,8)
+         preproc (Preprocessor): A TextPreprocessor or ImagePreprocessor.
+                                 For some data like text data, a preprocessor
+                                 is required to undo the pre-processing
+                                 to correctly view raw data.
+          val_data:  optional val_data to use instead of self.val_data
+        Returns:
+            list of n tuples where first element is either 
+            filepath or id of validation example and second element
+            is loss.
+
         """
-        raise NotImplementedError('view_top_losses must be overriden by GenLearner subclass')
+        val = self._check_val(val_data)
+
+
+        # get top losses and associated data
+        tups = self.top_losses(n=n, val_data=val, preproc=preproc)
+
+        # get multilabel status and class names
+        classes = preproc.get_classes() if preproc is not None else None
+        # iterate through losses
+        for tup in tups:
+
+            # get data
+            idx = tup[0]
+            loss = tup[1]
+            truth = tup[2]
+            pred = tup[3]
+
+            print('----------')
+            print("id:%s | loss:%s | true:%s | pred:%s)\n" % (idx, round(loss,2), truth, pred))
+        return
 
 
 
