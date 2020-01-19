9e106fa053be8889780b15239a78df569817b282
Arun Maiya <amaiya@ida.org>
2020-01-19 09:05:35 -0500

    add missing ktrain.data module

---
+++ ktrain/data.py
@@ -0,0 +1,71 @@
+from .imports import *
+
+
+class Dataset(Sequence):
+   def __init__(self, batch_size=32):
+       self.batch_size = batch_size
+
+   def __len__(self):
+       raise NotImplemented
+
+   def __getitem__(self, idx):
+       raise NotImplemented
+
+   def on_epoch_end(self):
+       raise NotImplemented
+
+   def xshape(self):
+       raise NotImplemented
+
+   def nsamples(self):
+       raise NotImplemented
+
+   def nclasses(self):
+       raise NotImplemented
+
+   def ondisk(self):
+       raise NotImplemented
+
+
+
+class ArrayDataset(Dataset):
+    def __init__(self, x, y, batch_size=32):
+        if type(x) != np.ndarray or type(y) != np.ndarray:
+            raise ValueError('x and y must be numpy arrays')
+        super().__init__(batch_size=batch_size)
+        self.x, self.y = x, y
+        self.indices = np.arange(self.x[0].shape[0])
+        if len(x.shape) == 2:
+            self.n_inputs = 1 
+        else:
+            self.n_inputs = x.shape[0]
+
+    def __len__(self):
+        return math.ceil(self.x[0].shape[0] / self.batch_size)
+
+    def __getitem__(self, idx):
+        inds = self.indices[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_x = []
+        for i in range(self.n_inputs):
+            batch_x.append(self.x[i][inds])
+        batch_y = self.y[inds]
+        return tuple(batch_x), batch_y
+
+    def on_epoch_end(self):
+        np.random.shuffle(self.indices)
+
+    def xshape(self):
+        return self.x.shape
+
+    def nsamples(self):
+        if self.n_inputs == 1:
+            return self.x.shape[0]
+        else:
+            return self.x.shape[1]
+
+    def nclasses(self):
+        return self.y.shape[1]
+
+    def ondisk(self):
+        return False
+
