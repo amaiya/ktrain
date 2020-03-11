493b56833853ba371f4876128fa225ba755a12ff
Arun Maiya <arun@maiya.net>
2020-03-11 12:33:46 -0400

    warning test

--- ktrain/text/shallownlp/__init__.py
+++ ktrain/text/shallownlp/__init__.py
@@ -3,12 +3,12 @@ import os, logging, warnings
 #os.environ['DISABLE_V2_BEHAVIOR'] = '1'
 
 
-# warnings controlled by ktrain.imports.SHOW_ALL_WARNINGS
+# warnings controlled by ktrain.imports.SHOW_ALL_WARNINGS also
 
-#os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-#logging.getLogger("tensorflow").setLevel(logging.CRITICAL)
-#logging.getLogger("tensorflow_hub").setLevel(logging.CRITICAL)
-#warnings.simplefilter(action='ignore', category=FutureWarning)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+logging.getLogger("tensorflow").setLevel(logging.CRITICAL)
+logging.getLogger("tensorflow_hub").setLevel(logging.CRITICAL)
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 try:
     import tensorflow as tf
