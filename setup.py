3aa18bf6f8ae8a2c0241d901f4d0d38633d83db1
Arun Maiya <arun@maiya.net>
2020-03-29 19:38:50 -0400

    keras_bert version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
           'matplotlib >= 3.0.0',
           'pandas >= 1.0.1',
           'fastprogress >= 0.1.21',
-          'keras_bert',
+          'keras_bert>=0.81.0',
           'requests',
           'joblib',
           'langdetect',
