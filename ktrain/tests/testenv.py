76c30e1e6cf46d56eaf3c5d1bced4630799a3047
Arun Maiya <arun@maiya.net>
2020-04-14 15:34:06 -0400

    testenv

--- ktrain/tests/testenv.py
+++ ktrain/tests/testenv.py
@@ -3,6 +3,6 @@ import os
 #os.environ['TF_EAGER'] = '0'
 import sys
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 sys.path.insert(0,'../..')
 
