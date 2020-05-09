4fd916b6f674a25b2b99fde09154f380165e2a39
Arun Maiya <arun@maiya.net>
2020-05-09 18:55:40 -0400

    minor edit

--- ktrain/vision/__init__.py
+++ ktrain/vision/__init__.py
@@ -1,5 +1,5 @@
 from .models import print_image_classifiers, image_classifier
-from .data import show_image, show_random_image, preview_data_aug, get_data_aug
+from .data import show_image, show_random_images, preview_data_aug, get_data_aug
 from .data import images_from_folder, images_from_csv, images_from_array, images_from_fname, preprocess_csv
 from .predictor import ImagePredictor
 __all__ = [
