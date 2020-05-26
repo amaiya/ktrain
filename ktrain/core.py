35492d2eef7736ab7bae30a2a19f1580b9f7fd09
Arun Maiya <arun@maiya.net>
2020-05-26 14:26:05 -0400

    changed warning text for loading models and preproces

--- ktrain/core.py
+++ ktrain/core.py
@@ -1332,7 +1332,7 @@ def load_predictor(fpath, batch_size=U.DEFAULT_BS):
     except:
         try:
             preproc_name = fpath +'.preproc'
-            warnings.warn('could load .preproc file as %s - attempting to load as %s' % (os.path.join(fpath, U.PREPROC_NAME), preproc_name))
+            warnings.warn('could not load .preproc file as %s - attempting to load as %s' % (os.path.join(fpath, U.PREPROC_NAME), preproc_name))
             with open(preproc_name, 'rb') as f: preproc = pickle.load(f)
         except:
             raise Exception('Could not find a .preproc file in either the post v0.16.x loction (%s) or pre v0.16.x location (%s)' % (os.path.join(fpath. U.PRERPC_NAME), fpath+'.preproc'))
