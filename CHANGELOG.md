e95a24f436c5a7489d5fc0c1f12c1919f3dfe1e7
Arun Maiya <arun@maiya.net>
2020-07-28 17:49:08 -0400

    added max_length parameter to zsl.predict

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -22,6 +22,7 @@ Most recent releases are shown at the top. Each release shows:
 - added `save_path` argument to `Learner.validate` and `Learner.evaluate`.  If `print_report=False`, classification
   report will be saved as CSV to `save_path`.
 - Use `torch.no_grad` with `ZeroShotClassifier.predict` to [prevent OOM](https://github.com/amaiya/ktrain/issues/215)
+- Added `max_length` parameter to `ZeroShotClassifier.predict` to [prevent errors on long documnets](https://github.com/amaiya/ktrain/issues/215)
 
 
 ## 0.18.5 (2020-07-20)
--- ktrain/text/zsl/core.py
+++ ktrain/text/zsl/core.py
@@ -27,7 +27,7 @@ class ZeroShotClassifier():
         self.model = AutoModelForSequenceClassification.from_pretrained(model_name).to(self.torch_device)
 
 
-    def predict(self, doc, topic_strings=[], include_labels=False, batch_size=8):
+    def predict(self, doc, topic_strings=[], include_labels=False, max_length=512, batch_size=8):
         """
         zero-shot topic classification
         Args:
@@ -40,6 +40,7 @@ class ZeroShotClassifier():
                                for your system, you should break up the topic_strings into 
                                chunks and invoke predict separately on each chunk.
           include_labels(bool): If True, will return topic labels along with topic probabilities
+          max_length(int): truncate long documents to this many tokens
           batch_size(int): batch_size to use. default:8
                            Increase this value to speed up predictions - especially
                            if len(topic_strings) is large.
@@ -47,27 +48,27 @@ class ZeroShotClassifier():
           inferred probabilities
         """
         import torch
-        with torch.no_grad():
-            if topic_strings is None or len(topic_strings) == 0:
-                raise ValueError('topic_strings must be a list of strings')
-            if batch_size > len(topic_strings): batch_size = len(topic_strings)
-            topic_chunks = list(U.list2chunks(topic_strings, n=math.ceil(len(topic_strings)/batch_size)))
-            if len(topic_strings) >= 100 and batch_size==8:
-                warnings.warn('TIP: Try increasing batch_size to speedup ZeroShotClassifier predictions')
-            result = []
-            for topics in topic_chunks:
-                pairs = []
-                for topic_string in topics:
-                    premise = doc
-                    hypothesis = 'This text is about %s.' % (topic_string)
-                    pairs.append( (premise, hypothesis) )
-                batch = self.tokenizer.batch_encode_plus(pairs, return_tensors='pt', padding='longest').to(self.torch_device)
+        if topic_strings is None or len(topic_strings) == 0:
+            raise ValueError('topic_strings must be a list of strings')
+        if batch_size > len(topic_strings): batch_size = len(topic_strings)
+        topic_chunks = list(U.list2chunks(topic_strings, n=math.ceil(len(topic_strings)/batch_size)))
+        if len(topic_strings) >= 100 and batch_size==8:
+            warnings.warn('TIP: Try increasing batch_size to speedup ZeroShotClassifier predictions')
+        result = []
+        for topics in topic_chunks:
+            pairs = []
+            for topic_string in topics:
+                premise = doc
+                hypothesis = 'This text is about %s.' % (topic_string)
+                pairs.append( (premise, hypothesis) )
+            batch = self.tokenizer.batch_encode_plus(pairs, return_tensors='pt', max_length=max_length, truncation='only_first', padding=True).to(self.torch_device)
+            with torch.no_grad():
                 logits = self.model(batch['input_ids'], attention_mask=batch['attention_mask'])[0]
-                entail_contradiction_logits = logits[:,[0,2]]
-                probs = entail_contradiction_logits.softmax(dim=1)
-                true_probs = list(probs[:,1].cpu().detach().numpy())
-                if include_labels:
-                    true_probs = list(zip(topics, true_probs))
-                result.extend(true_probs)
-            return result
+            entail_contradiction_logits = logits[:,[0,2]]
+            probs = entail_contradiction_logits.softmax(dim=1)
+            true_probs = list(probs[:,1].cpu().detach().numpy())
+            if include_labels:
+                true_probs = list(zip(topics, true_probs))
+            result.extend(true_probs)
+        return result
 
