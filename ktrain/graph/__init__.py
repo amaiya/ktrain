8c6348ba5cebe8b1fa1a0f0f4a49553743bd1c5b
Sandy Khosasi <45941585+ilos-vigil@users.noreply.github.com>
2020-09-16 15:50:03 +0700

    Fix typo in  __all__

--- ktrain/graph/__init__.py
+++ ktrain/graph/__init__.py
@@ -2,11 +2,11 @@ from .models import *
 from .data import *
 #from .predictor import *
 __all__ = [
-           'graph_nodes_from_csv'
-           'graph_links_from_csv'
+           'graph_nodes_from_csv',
+           'graph_links_from_csv',
            'print_node_classifiers',
            'print_link_predictors',
-           'node_classifier'
-           'link_predictor'
+           'graph_node_classifier',
+           'graph_link_predictor'
            ]
 
