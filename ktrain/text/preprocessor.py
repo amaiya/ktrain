ebaa8da99d080301286736baecdb7eae0f9f8b41
Arun Maiya <arun@maiya.net>
2020-10-27 10:17:24 -0400

    use fast tokenizers for bert, distilbert, and roberta

--- ktrain/text/preprocessor.py
+++ ktrain/text/preprocessor.py
@@ -833,7 +833,19 @@ class TransformersPreprocessor(TextPreprocessor):
     def get_tokenizer(self, fpath=None):
         model_name = self.model_name if fpath is None else fpath
         if self.tok is None:
-            self.tok = self.tokenizer_type.from_pretrained(model_name)
+            # use fast tokenizer if possible
+            if self.name == 'bert':
+                from transformers import BertTokenizerFast
+                self.tok = BertTokenizerFast.from_pretrained(model_name)
+            elif self.name == 'distilbert':
+                from transformers import DistilBertTokenizerFast
+                self.tok = DistilBertTokenizerFast.from_pretrained(model_name)
+            elif self.name == 'roberta':
+                from transformers import RobertaTokenizerFast
+                self.tok = RobertaTokenizerFast.from_pretrained(model_name)
+            else:
+                self.tok = self.tokenizer_type.from_pretrained(model_name)
+        print(self.tok)
         return self.tok
 
 
