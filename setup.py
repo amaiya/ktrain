fa073c0a6ea3dfbf391af3d17f606022d9935927
Arun Maiya <arun@maiya.net>
2021-10-17 17:26:10 -0400

    test comment

--- setup.py
+++ setup.py
@@ -50,8 +50,8 @@ setup(
         # NOTE: If missing, these libraries below are installed manually on-the-fly when required by an invoked method with appropriate warnings
         'all': [  'ipython', 
                   'torch==1.8.1',
-                  'stellargraph @ git+https://github@github.com/amaiya/stellargraph@no_tf_dep_082#egg=stellargraph'
-                  'eli5 @ git+https://github@github.com/amaiya/eli5@tfkeras_0_10_1#egg=eli5',
+                  #'stellargraph @ git+https://github@github.com/amaiya/stellargraph@no_tf_dep_082#egg=stellargraph'
+                  #'eli5 @ git+https://github@github.com/amaiya/eli5@tfkeras_0_10_1#egg=eli5',
                   'shap',
                   'causalnlp',
                   'textract'
@@ -59,14 +59,16 @@ setup(
                   ],
         # graph module
         'graph': [ 'networkx>=2.3',
-                   'stellargraph @ git+https://github@github.com/amaiya/stellargraph@no_tf_dep_082#egg=stellargraph'],
+                   #'stellargraph @ git+https://github@github.com/amaiya/stellargraph@no_tf_dep_082#egg=stellargraph'
+                   ],
         # text, vision, and tabular explain
         'explain': [ 'shap',
-                     'eli5 @ https://files.pythonhosted.org/packages/05/dc/ae333ca238bf3809164f6dfef42f75d2199287f1db7c93425db6c1f4af7d/eli5-0.10.1.tar.gz'],
+                     #'eli5 @ https://files.pythonhosted.org/packages/05/dc/ae333ca238bf3809164f6dfef42f75d2199287f1db7c93425db6c1f4af7d/eli5-0.10.1.tar.gz'
+                     ],
         # tabularl.causalinference
         'causal': [ 'causalnlp'],
         # qa.SimpleQA text extraction
-        'text_extraction': [ 'textract'],
+        'text_extraction': ['textract'],
         # not included and checked/requested within-code: 
         # 1. bokeh: in TopicModel.visualize_docuemnts
         # 2. datasets: in QAFineTuner.finetune
