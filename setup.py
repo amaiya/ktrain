2ce08344c291d95d6405bd3d0e42ce5530f8e0a0
Arun Maiya <arun@maiya.net>
2021-10-17 20:28:46 -0400

    minor edit

--- setup.py
+++ setup.py
@@ -48,12 +48,12 @@ setup(
       ],
     extras_require={
         # NOTE: If missing, these libraries below are installed manually on-the-fly when required by an invoked method with appropriate warnings
-        'tests': [ 'ipython',         # for tests
-                  'torch==1.8.1',     # for qa, summarization, translation, zsl
-                  'shap',             # for tabular.TabularPredictor.explain
-                  'causalnlp',        # for tabular.causalinference
-                  'textract',         # for TextExtractor
-                  'datasets',         # for text.qa.AnswerExtractor.finetune
+        'tests': [ 'ipython',        # for tests
+                  'torch==1.8.1',    # for qa, summarization, translation, zsl
+                  'shap',            # for tabular.TabularPredictor.explain
+                  'causalnlp',       # for tabular.causalinference
+                  'textract',        # for TextExtractor
+                  'datasets',        # for text.qa.AnswerExtractor.finetune
                   'stellargraph @ git+https://github@github.com/amaiya/stellargraph@no_tf_dep_082#egg=stellargraph', # for graph module
                   'eli5 @ git+https://github@github.com/amaiya/eli5@tfkeras_0_10_1#egg=eli5',                        # for explain in text/vision
                   ],
