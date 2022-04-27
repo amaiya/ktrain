196c72f86ccdf43ea53bd401b6db0c1772202809
Arun Maiya <arun@maiya.net>
2022-04-27 13:55:39 -0400

    removed sentence pair warning to avoid confusion

--- ktrain/text/preprocessor.py
+++ ktrain/text/preprocessor.py
@@ -239,16 +239,16 @@ def _is_sentence_pair(tup):
     ):
         return True
     else:
-        if (
-            isinstance(tup, (list, np.ndarray))
-            and len(tup) == 2
-            and isinstance(tup[0], str)
-            and isinstance(tup[1], str)
-        ):
-            warnings.warn(
-                "List or array of two texts supplied, so task being treated as text classification. "
-                + "If this is a sentence pair classification task, please cast to tuple."
-            )
+        # if (
+        # isinstance(tup, (list, np.ndarray))
+        # and len(tup) == 2
+        # and isinstance(tup[0], str)
+        # and isinstance(tup[1], str)
+        # ):
+        # warnings.warn(
+        # "List or array of two texts supplied, so task being treated as text classification. "
+        # + "If this is a sentence pair classification task, please cast to tuple."
+        # )
         return False
 
 
