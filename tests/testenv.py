644c80014976415cdf148950dcea1aa02b4816cf
Arun Maiya <arun@maiya.net>
2022-05-15 18:35:19 -0400

    testenv test

--- tests/testenv.py
+++ tests/testenv.py
@@ -1,8 +1,8 @@
 import os
-# os.environ['TF_KERAS'] = '1'
-# os.environ['TF_EAGER'] = '0'
+import os.path
 import sys
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-sys.path.insert(0, "../..")
+CURRDIR = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(CURRDIR, ".."))
