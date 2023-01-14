7e4a5effe973435f62a9be644ddd8d0b9f75d1cf
Arun S. Maiya <arun@maiya.net>
2023-01-14 09:16:01 -0500

    fixes 476

--- ktrain/text/preprocessor.py
+++ ktrain/text/preprocessor.py
@@ -906,6 +906,10 @@ class TransformersPreprocessor(TextPreprocessor):
             self.ytransform = U.YTransform(
                 class_names=self.get_classes(), label_encoder=le
             )
+        if not hasattr(self, 'use_token_type_ids'):
+            # As a short cut, we simply set use_token_type_ids to False if model is distilbert,
+            # as most models use token_type_ids (e.g., bert, deberta, etc.).
+            self.use_token_type_ids = self.name != 'distilbert'
 
     def set_config(self, config):
         self.config = config
