42844d779aed40891ddafe767094bf9af9b609b9
Arun S. Maiya <arun@maiya.net>
2023-11-18 12:18:49 -0500

    fixes #515

--- setup.py
+++ setup.py
@@ -83,10 +83,10 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
